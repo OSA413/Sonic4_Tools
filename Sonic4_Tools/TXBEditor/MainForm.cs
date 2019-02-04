61f243fc7455b822a7e724170fe758eb2b8c8260
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2019-02-05 00:19:11 +0500

    Added sketch for TXBEditor

---
+++ Sonic4_Tools/TXBEditor/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TXBEditor
+{
+    public partial class MainForm:Form
+    {
+        public List<byte[]> Values { get; set; }
+
+        static string DirectorySelectionDialog()
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "TXB|*.TXB|All files|*.*";
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+                ofd.Title = "Select a TXB file";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    return ofd.FileName;
+                }
+            }
+            return null;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateValues()
+        {
+            cb_Num.Items.Clear();
+            for (int i = 0; i < Values.Count; i++)
+            {
+                cb_Num.Items.Add(i);
+            }
+            
+            if (Values.Count > 0)
+            { cb_Num.SelectedIndex = 0; }
+        }
+
+        private void b_Open_Click(object sender, EventArgs e)
+        {
+            string tmp_file = DirectorySelectionDialog();
+            if (tmp_file != null)
+            { 
+                l_File.Text = tmp_file;
+                Values = TXB.Read(tmp_file);
+                UpdateValues();
+            }
+        }
+    }
+}
---
+++ Sonic4_Tools/TXBEditor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TXBEditor
+{
+    public static class TXB
+    {
+        public static List<byte[]> Read(byte[] RawFile)
+        {
+            int FileNumber = BitConverter.ToInt32(RawFile, 0x10);
+            if (BitConverter.IsLittleEndian)
+            { FileNumber = BitConverter.ToInt32(BitConverter.GetBytes(FileNumber).Reverse().ToArray(), 0); }
+
+            List<byte[]> Return = new List<byte[]>();
+            
+            if (FileNumber > 0)
+            {
+                int NamePointer = BitConverter.ToInt32(RawFile, 0x1C);
+                if (BitConverter.IsLittleEndian)
+                { NamePointer = BitConverter.ToInt32(BitConverter.GetBytes(NamePointer).Reverse().ToArray(), 0); }
+
+                for (int i = 0; i < (NamePointer - 0x20) / 4; i++)
+                {
+                    Return.Add(RawFile.Skip(0x20 + i * 4).Take(4).ToArray());
+                }
+            }
+
+            return Return;
+        }
+
+        public static List<byte[]> Read(string FileName)
+        {
+            return TXB.Read(File.ReadAllBytes(FileName));
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
