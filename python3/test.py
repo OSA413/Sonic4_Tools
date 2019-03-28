80726480e1516e943f9e847c4eab47f28c43b63b
OSA413 <OSA413@users.noreply.github.com>
2019-03-28 22:08:02 +0500

    Updated useless leftover

--- python3/test.py
+++ python3/test.py
@@ -1,3 +1,5 @@
+swap_end = False
+
 import struct
 import matplotlib.pyplot as plt
 
@@ -11,13 +13,20 @@ def byte2int(byte, swap_endianness=False):
     if swap_endianness: end = -1
     return struct.unpack('i', byte[::end])[0]
 
-with open(r"D:\Steam\steamapps\common\Sonic the Hedgehog 4 EP 1\mods\tes\G_ZONE1\MAP\ZONE11_MAP.AMB\OUT\EL\ZONE11.EV", "rb") as f:
+file_name = input("Enter file name>>> ")
+
+#removing quotes and space from ubuntu's drag&drop in terminal
+if file_name[0] == file_name[-2] == "\'" and file_name[-1] == " ":
+    print("+")
+    file_name = file_name[1:-2]
+
+with open(file_name, "rb") as f:
     a = f.read()
 
 c = []
 
 for i in range(len(a)//4):
-    b = byte2int(a[i*4:i*4+4],True)
+    b = byte2int(a[i*4:i*4+4], swap_end)
     if not b in c:
         c.append(b)
         plt.scatter(b, 0)
