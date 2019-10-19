30b9557669626b21181bdce04323b6cdd3bab975
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2019-10-20 02:13:56 +0500

    some code review

--- Sonic4_Tools/OldModConversionTool/MainForm.cs
+++ Sonic4_Tools/OldModConversionTool/MainForm.cs
@@ -2,101 +2,39 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
-using System.Security.Cryptography;
 using System.Linq;
-using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace OldModConversionTool
 {
     public partial class MainForm:Form
     {
-        public void Settings_Save()
+        public void SettingsSave()
         {
-            string[] text = new string[]
-            {
-                "LastMod="    + tbModPath.Text,
-                "LastGame="   + tbGamePath.Text,
-                "LastOutput=" + tbOutputPath.Text,
-            };
+            Settings.LastMod = tbModPath.Text;
+            Settings.LastGame = tbGamePath.Text;
+            Settings.LastOutput = tbOutputPath.Text;
 
-            File.WriteAllLines("omct.cfg", text);
+            Settings.Save();
         }
 
-        public void Settings_Load()
+        public void SettingsLoad()
         {
-            if (!File.Exists("omct.cfg")) {return;}
+            Settings.Load();
 
-            string[] cfg_file = File.ReadAllLines("omct.cfg");
-
-            foreach (string line in cfg_file)
-            {
-                if (!line.Contains("=")) {continue;}
-
-                string formatted_line = line.Substring(line.IndexOf("=") + 1);
-
-                if (line.StartsWith("LastMod="))
-                { tbModPath.Text    = formatted_line; }
-
-                else if (line.StartsWith("LastGame="))
-                { tbGamePath.Text   = formatted_line; }
-                
-                else if (line.StartsWith("LastOutput="))
-                { tbOutputPath.Text = formatted_line; }
-            }
+            tbModPath.Text    = Settings.LastMod;
+            tbGamePath.Text   = Settings.LastGame;
+            tbOutputPath.Text = Settings.LastOutput;
         }
         
-        static void File_Delete(string file)
-        {
-            //Program will crash if it tries to delete a read-only file
-            File.SetAttributes(file, FileAttributes.Normal);
-            File.Delete(file);
-        }
-
-        static void DirectoryRemoveRecursively(string dir)
-        {
-            foreach (string file in Directory.GetFiles(dir))
-            {
-                File_Delete(file);
-            }
-
-            foreach (string dirr in Directory.GetDirectories(dir))
-            {
-                DirectoryRemoveRecursively(dirr);
-            }
-            Directory.Delete(dir);
-        }
-
         public MainForm()
         {
             InitializeComponent();
             RefreshStatus();
             IsReady();
-            Settings_Load();
+            SettingsLoad();
         }
-
-        static string Sha(string file)
-        {
-            byte[] hash;
-            byte[] raw_file = File.ReadAllBytes(file);
-            string str_hash = "";
-            
-            hash = new SHA512CryptoServiceProvider().ComputeHash(raw_file);
-            
-            foreach (byte b in hash) { str_hash += b.ToString("X"); }
-
-            return str_hash;
-        }
-
-        static void RemoveEmptyDirs(string parent_dir)
-        {
-            foreach (string dir in Directory.GetDirectories(parent_dir))
-            { RemoveEmptyDirs(dir); }
-
-            if (Directory.GetFileSystemEntries(parent_dir).Length == 0)
-            { DirectoryRemoveRecursively(parent_dir); }
-        }
-
+        
         private void RefreshStatus()
         {
             //AMBPatcher
@@ -116,17 +54,11 @@ namespace OldModConversionTool
                 lCsbEditorStatus.ForeColor = Color.Green;
             }
             else if (File.Exists("Common/CsbEditor.exe"))
-            {
                 lCsbEditorStatus.Text = "SonicAudioLib.dll is missing";
-            }
             else if (File.Exists("Common/SonicAudioLib.dll"))
-            {
                 lCsbEditorStatus.Text = "CsbEditor.exe is missing";
-            }
             else
-            {
                 lCsbEditorStatus.Text = "CsbEditor.exe and SonicAudioLib.dll are missing";
-            }
         }
 
         private void IsReady()
@@ -138,31 +70,7 @@ namespace OldModConversionTool
                 bConvert.Enabled = true;
             }
         }
-
-        static string DirectorySelectionDialog(int type)
-        {
-            using (var ofd = new OpenFileDialog())
-            {
-                ofd.ValidateNames = false;
-                ofd.CheckFileExists = false;
-                ofd.CheckPathExists = true;
-                ofd.FileName = "[DIRECTORY]";
-
-                switch (type)
-                {
-                    case 0: ofd.Title = "Select the root directory of your mod"; break;
-                    case 1: ofd.Title = "Select the root directory the game"; break;
-                    case 2: ofd.Title = "Select the output directory"; break;
-                }
-
-                if (ofd.ShowDialog() == DialogResult.OK)
-                {
-                    return Path.GetDirectoryName(ofd.FileName);
-                }
-            }
-            return null;
-        }
-
+        
         private void UnpackFiles(string folder, string mod_path, string output_path, string game_path)
         {
             statusBar.Text = "Comparing files in \"" + folder + "\"...";
@@ -171,19 +79,19 @@ namespace OldModConversionTool
             //Think up a comment that describes what this block does
             string the_orig_path;
             if (Directory.Exists(Path.Combine(output_path, "orig", folder)))
-            { the_orig_path = Path.Combine(output_path, "orig", folder); }
+                the_orig_path = Path.Combine(output_path, "orig", folder);
             else
-            { the_orig_path = Path.Combine(game_path, folder); }
+                the_orig_path = Path.Combine(game_path, folder);
 
             string[] mod_files = Directory.GetFiles(Path.Combine(mod_path, folder), "*", SearchOption.AllDirectories);
 
             string[] game_files = Directory.GetFiles(the_orig_path, "*", SearchOption.AllDirectories);
 
             for (int i = 0; i < game_files.Length; i++)
-            { game_files[i] = game_files[i].Substring(the_orig_path.Length + 1); }
+                game_files[i] = game_files[i].Substring(the_orig_path.Length + 1);
 
             for (int i = 0; i < mod_files.Length; i++)
-            { mod_files[i] = mod_files[i].Substring(mod_path.Length + folder.Length + 2); }
+                mod_files[i] = mod_files[i].Substring(mod_path.Length + folder.Length + 2);
 
             foreach (string file in mod_files)
             {
@@ -195,12 +103,11 @@ namespace OldModConversionTool
                     string output_orig_file = Path.Combine(output_path, "orig", folder, file);
                     string output_mod_file = Path.Combine(output_path, folder, file);
 
-                    if (!File.Exists(output_mod_file))
-                    { continue; }
+                    if (!File.Exists(output_mod_file)) continue;
 
-                    if (Sha(output_mod_file) == Sha(orig_file))
+                    if (SHA.GetSHA512(output_mod_file) == SHA.GetSHA512(orig_file))
                     {
-                        File_Delete(output_mod_file);
+                        MyFile.Delete(output_mod_file);
                         check_again = true;
                     }
                     else
@@ -213,38 +120,23 @@ namespace OldModConversionTool
 
                         if (file.EndsWith(".AMB", StringComparison.OrdinalIgnoreCase))
                         {
-                            ProcessStartInfo startInfo = new ProcessStartInfo
-                            {
-                                FileName = Path.Combine("Common", "AMBPatcher.exe"),
-                                Arguments = output_mod_file,
-                                WindowStyle = ProcessWindowStyle.Hidden
-                            };
-
-                            Process.Start(startInfo).WaitForExit();
-                            File_Delete(output_mod_file);
+                            Extract.AMB(output_mod_file);
+                            MyFile.Delete(output_mod_file);
                             Directory.Move(output_mod_file + "_extracted", output_mod_file);
 
-                            startInfo.Arguments = output_orig_file;
-                            Process.Start(startInfo).WaitForExit();
-                            File_Delete(output_orig_file);
+                            Extract.AMB(output_orig_file);
+                            MyFile.Delete(output_orig_file);
                             Directory.Move(output_orig_file + "_extracted", output_orig_file);
 
                             check_again = true;
                         }
                         else if (file.EndsWith(".CSB", StringComparison.OrdinalIgnoreCase))
                         {
-                            ProcessStartInfo startInfo = new ProcessStartInfo
-                            {
-                                FileName = Path.Combine("Common", "CsbEditor.exe"),
-                                Arguments = output_mod_file,
-                                WindowStyle = ProcessWindowStyle.Hidden
-                            };
-                            Process.Start(startInfo).WaitForExit();
-                            File_Delete(output_mod_file);
+                            Extract.CSB(output_mod_file);
+                            MyFile.Delete(output_mod_file);
 
-                            startInfo.Arguments = output_orig_file;
-                            Process.Start(startInfo).WaitForExit();
-                            File_Delete(output_orig_file);
+                            Extract.CSB(output_orig_file);
+                            MyFile.Delete(output_orig_file);
 
                             check_again = true;
                         }
@@ -259,25 +151,17 @@ namespace OldModConversionTool
                                 File.Copy(orig_file, output_orig_file);
                                 File.Copy(orig_file, output_mod_file);
 
-                                ProcessStartInfo startInfo = new ProcessStartInfo
-                                {
-                                    FileName = Path.Combine("Common", "CsbEditor.exe"),
-                                    Arguments = output_mod_file,
-                                    WindowStyle = ProcessWindowStyle.Hidden
-                                };
-                                Process.Start(startInfo).WaitForExit();
-                                File_Delete(output_mod_file);
-                                File_Delete(output_mod_file.Substring(0, output_mod_file.Length - 4) + ".CPK");
+                                Extract.CSB(output_mod_file);
+                                MyFile.Delete(output_mod_file);
+                                MyFile.Delete(output_mod_file.Substring(0, output_mod_file.Length - 4) + ".CPK");
 
-                                startInfo.Arguments = output_orig_file;
-                                Process.Start(startInfo).WaitForExit();
-                                File_Delete(output_orig_file);
-                                File_Delete(output_orig_file.Substring(0, output_orig_file.Length - 4) + ".CPK");
+                                Extract.CSB(output_orig_file);
+                                MyFile.Delete(output_orig_file);
+                                MyFile.Delete(output_orig_file.Substring(0, output_orig_file.Length - 4) + ".CPK");
 
                                 check_again = true;
                             }
                         }
-
                     }
                 }
             }
@@ -293,21 +177,19 @@ namespace OldModConversionTool
             await Task.Run(() => 
             {
                 statusBar.Text = "Getting list of mod files...";
-
                 string[] mod_files = Directory.GetFiles(tbModPath.Text, "*", SearchOption.AllDirectories);
                 for (int i = 0; i < mod_files.Length; i++)
-                { mod_files[i] = mod_files[i].Substring(tbModPath.Text.Length + 1); }
+                    mod_files[i] = mod_files[i].Substring(tbModPath.Text.Length + 1);
 
                 statusBar.Text = "Getting list of game files...";
-
                 string[] game_files = Directory.GetFiles(tbGamePath.Text, "*", SearchOption.AllDirectories);
                 for (int i = 0; i < game_files.Length; i++)
-                { game_files[i] = game_files[i].Substring(tbGamePath.Text.Length + 1); }
+                    game_files[i] = game_files[i].Substring(tbGamePath.Text.Length + 1);
                 
                 if (Directory.Exists(tbOutputPath.Text))
                 {
                     statusBar.Text = "Removing output directory...";
-                    DirectoryRemoveRecursively(tbOutputPath.Text);
+                    MyDirectory.RemoveRecursively(tbOutputPath.Text);
                 }
                 
                 foreach (string file in mod_files)
@@ -324,10 +206,10 @@ namespace OldModConversionTool
                 }
 
                 statusBar.Text = "Removing temp files...";
-                DirectoryRemoveRecursively(Path.Combine(tbOutputPath.Text, "orig"));
+                MyDirectory.RemoveRecursively(Path.Combine(tbOutputPath.Text, "orig"));
 
                 statusBar.Text = "Removing empty directories...";
-                RemoveEmptyDirs(tbOutputPath.Text);
+                MyDirectory.RemoveEmptyDirs(tbOutputPath.Text);
 
                 statusBar.Text = "Done";
 
@@ -347,46 +229,34 @@ namespace OldModConversionTool
                     }));
                 }
                 
-
-                string local_explorer = "";
-                switch ((int) Environment.OSVersion.Platform)
-                {
-                    //Windows
-                    case 2: local_explorer = "explorer"; break;
-                    //Linux (with xdg)
-                    case 4: local_explorer = "xdg-open"; break;
-                    //MacOS (not tested)
-                    case 6: local_explorer = "open"; break;
-                }
-
-                Process.Start(local_explorer, tbOutputPath.Text);
+                MyDirectory.OpenInExplorer(tbOutputPath.Text);
             });
         }
 
         private void bModPath_Click(object sender, EventArgs e)
         {
-            string path = DirectorySelectionDialog(0);
+            string path = MyDirectory.SelectionDialog(0);
             if (path != null)
-            { tbModPath.Text = path; }
+                tbModPath.Text = path;
         }
 
         private void bGamePath_Click(object sender, EventArgs e)
         {
-            string path = DirectorySelectionDialog(1);
+            string path = MyDirectory.SelectionDialog(1);
             if (path != null)
-            { tbGamePath.Text = path; }
+                tbGamePath.Text = path;
         }
 
         private void bOutputPath_Click(object sender, EventArgs e)
         {
-            string path = DirectorySelectionDialog(2);
+            string path = MyDirectory.SelectionDialog(2);
             if (path != null)
-          { tbOutputPath.Text = path; }
+                tbOutputPath.Text = path;
         }
 
         private void bConvert_Click(object sender, EventArgs e)
         {
-            Settings_Save();
+            SettingsSave();
 
             statusBar.Text = "Checking directory existence...";
 
@@ -396,8 +266,8 @@ namespace OldModConversionTool
 
             if (!(Directory.Exists(tbModPath.Text) && Directory.Exists(tbGamePath.Text)))
             {
-                if (!Directory.Exists(tbModPath.Text)) { tbModPath.BackColor = Color.FromArgb(255, 192, 192); }
-                if (!Directory.Exists(tbGamePath.Text)) { tbGamePath.BackColor = Color.FromArgb(255, 192, 192); }
+                if (!Directory.Exists(tbModPath.Text))  tbModPath.BackColor  = Color.FromArgb(255, 192, 192);
+                if (!Directory.Exists(tbGamePath.Text)) tbGamePath.BackColor = Color.FromArgb(255, 192, 192);
                 MessageBox.Show("The mod root directory and/or the game root directory not found.", "Directory not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
--- Sonic4_Tools/OldModConversionTool/Program.cs
+++ Sonic4_Tools/OldModConversionTool/Program.cs
@@ -1,10 +1,166 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows.Forms;
-
-//TODO rewrite
 
 namespace OldModConversionTool
 {
+    public static class Settings
+    {
+        public static string LastMod;
+        public static string LastGame;
+        public static string LastOutput;
+
+        public static void Save()
+        {
+            string[] text = new string[]
+            {
+                "LastMod="    + LastMod,
+                "LastGame="   + LastGame,
+                "LastOutput=" + LastOutput,
+            };
+
+            File.WriteAllLines("omct.cfg", text);
+        }
+
+        public static void Load()
+        {
+            LastMod = LastGame = LastOutput = "";
+
+            if (!File.Exists("omct.cfg")) return;
+
+            string[] cfg_file = File.ReadAllLines("omct.cfg");
+
+            foreach (string line in cfg_file)
+            {
+                if (!line.Contains("=")) continue;
+                string key   = line.Substring(0, line.IndexOf("="));
+                string value = line.Substring(line.IndexOf("=") + 1);
+
+                switch (key)
+                {
+                    case "LastMod":     LastMod     = value; break;
+                    case "LastGame":    LastGame    = value; break;
+                    case "LastOutput":  LastOutput  = value; break;
+                }
+            }
+        }
+    }
+    
+    public static class MyFile
+    {
+        public static void Delete(string file)
+        {
+            //Program will crash if it tries to delete a read-only file
+            File.SetAttributes(file, FileAttributes.Normal);
+            File.Delete(file);
+        }
+    }
+
+    public static class Extract
+    {
+        public static void AMB(string file)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = Path.Combine("Common", "AMBPatcher.exe"),
+                Arguments = file,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(startInfo).WaitForExit();
+        }
+
+        public static void CSB(string file)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = Path.Combine("Common", "CsbEditor.exe"),
+                Arguments = file,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(startInfo).WaitForExit();
+        }
+    }
+
+    public static class MyDirectory
+    {
+        public static void RemoveRecursively(string dir)
+        {
+            foreach (string file in Directory.GetFiles(dir))
+                MyFile.Delete(file);
+
+            foreach (string dirr in Directory.GetDirectories(dir))
+                MyDirectory.RemoveRecursively(dirr);
+
+            Directory.Delete(dir);
+        }
+
+        public static void RemoveEmptyDirs(string parent_dir)
+        {
+            foreach (string dir in Directory.GetDirectories(parent_dir))
+            { RemoveEmptyDirs(dir); }
+
+            if (Directory.GetFileSystemEntries(parent_dir).Length == 0)
+                MyDirectory.RemoveRecursively(parent_dir);
+        }
+
+        public static string SelectionDialog(int type)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.ValidateNames = false;
+                ofd.CheckFileExists = false;
+                ofd.CheckPathExists = true;
+                ofd.FileName = "[DIRECTORY]";
+
+                switch (type)
+                {
+                    case 0: ofd.Title = "Select the root directory of your mod"; break;
+                    case 1: ofd.Title = "Select the root directory the game"; break;
+                    case 2: ofd.Title = "Select the output directory"; break;
+                }
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    return Path.GetDirectoryName(ofd.FileName);
+                }
+            }
+            return null;
+        }
+
+        public static void OpenInExplorer(string folder)
+        {
+            string local_explorer = "";
+            switch ((int)Environment.OSVersion.Platform)
+            {
+                //Windows
+                case 2: local_explorer = "explorer"; break;
+                //Linux (with xdg)
+                case 4: local_explorer = "xdg-open"; break;
+                //MacOS (not tested)
+                case 6: local_explorer = "open"; break;
+            }
+            Process.Start(local_explorer, folder);
+        }
+    }
+
+    public static class SHA
+    {
+        public static string GetSHA512(string file)
+        {
+            byte[] hash;
+            byte[] raw_file = File.ReadAllBytes(file);
+            string str_hash = "";
+
+            hash = new SHA512CryptoServiceProvider().ComputeHash(raw_file);
+
+            foreach (byte b in hash) { str_hash += b.ToString("X"); }
+
+            return str_hash;
+        }
+    }
+
     static class Program
     {
         /// <summary>
